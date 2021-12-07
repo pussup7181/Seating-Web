98ca68e9af29ad6acc9e0f7c7068e9532625ebb2
pushp <psdamncool7@gmail.com>
2021-12-07 16:06:22 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIhjeL8NcDUSOqqOo9sEOIj2d1npUuOQ8",
+  authDomain: "friendlychat-a001c.firebaseapp.com",
+  projectId: "friendlychat-a001c",
+  storageBucket: "friendlychat-a001c.appspot.com",
+  messagingSenderId: "297611526873",
+  appId: "1:297611526873:web:c5f00ac07a1db3b2c5b4c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
