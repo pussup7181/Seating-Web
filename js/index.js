d423c865d9ed2ecd63d063b90f4c7ca56d8245d5
pushp <psdamncool7@gmail.com>
2021-12-07 16:26:03 +0530

    sign up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getDatabase, ref, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js";// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -14,4 +15,96 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase();
+  const auth = getAuth();
+
+  var nam = document.getElementById("name");
+  var designation = document.getElementById("designation");
+  var floor = document.getElementById("floor");
+  var type = document.getElementById("seat-type");
+  var number = document.getElementById("seat-num");
+  
+  var seatNumber;
+  var add = document.getElementById("add");
+  var email = document.getElementById("signup-email");
+  var password = document.getElementById("signup-password");
+  var signUpButton = document.getElementById("signUp-button").addEventListener('click',(e)=>{
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth,email.value,password.value)
+      .then((cred)=>{
+        console.log(cred);
+        window.location.href = "/index.html";
+      });
+  });
+
+
+
+
+  function GetSeat(f,t,n){
+    var seat = null;
+
+  switch (f)
+  {
+      case "GROUND FLOOR":
+          seat = "G";
+          break;
+      case "FIRST FLOOR":
+          seat = "F";
+          break;
+      case "SECOND FLOOR":
+          seat = "S";
+          break;
+  }
+
+  switch (t)
+  {
+    case "Chamber":
+          seat += "CH";
+          break;
+      case "Cabin":
+          seat += "C";
+          break;
+      case "Enclosure":
+          seat += "E";
+          break;
+      case "Workstation":
+          seat += "W";
+          break;
+  }
+  return seat + n;
+  }
+
+  function CheckData(){
+    seatNumber = GetSeat(floor.options[floor.selectedIndex].text, type.options[type.selectedIndex].text, number.value);
+    const dbref = ref(db);
+    get(child(dbref, "SeatNumber/"+seatNumber)).then((snapshot)=>{
+      if(snapshot.exists()){
+        alert("Seat Taken");   
+      }
+      else{
+        SaveData();
+      }
+    })
+    .catch((error)=>{
+      console.log(error);
+    });
+  }
+  button.addEventListener('click',CheckData);
+
+
+  function SaveData(){
+   seatNumber = GetSeat(floor.options[floor.selectedIndex].text, type.options[type.selectedIndex].text, number.value);
+    set(ref(db, "SeatNumber/"+seatNumber),{
+      name:nam.value,
+      designation:designation.value,
+      floor:floor.options[floor.selectedIndex].text,
+      seatNumber:seatNumber
+    })
+    .then(()=>{
+      alert("Data Successfully uploaded");
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
